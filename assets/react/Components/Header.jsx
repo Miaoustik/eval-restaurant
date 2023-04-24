24f9814957139d00623b9951f93ea733c14408f0
Miaoustik <cedricwat@outlook.fr>
2023-04-24 20:23:48 +0200

    Front: working on css header

--- assets/react/Components/Header.jsx
+++ assets/react/Components/Header.jsx
@@ -1,9 +1,8 @@
 import React, {useState} from "react";
 import styled from "styled-components";
-import {Button, Offcanvas, OffcanvasBody, OffcanvasHeader, OffcanvasTitle} from "react-bootstrap";
-import NavbarToggle from "react-bootstrap/NavbarToggle";
+import {Offcanvas, OffcanvasBody, OffcanvasHeader, OffcanvasTitle} from "react-bootstrap";
 
-export default function () {
+export default function ({ light = '0' }) {
 
     const [show, setShow] = useState(false);
 
@@ -12,48 +11,111 @@ export default function () {
 
     return (
         <header>
-            <Nav className={'d-flex gap-1 navbar  navbar-expdand-lg border-bottom border-primary border-1 pe-2'} >
-                <div className={"plate plate1 " + (show ? ' active' : '')}  onClick={handleShow}>
-                    <svg className="burger" version="1.1" height="100" width="100" viewBox="0 0 100 100">
+            <nav>
+                <StyledOffCanvas light={light} scroll={true} show={show} onHide={handleShow} className={"d-flex justify-content-center"}>
+                    <OffcanvasHeader className={'mx-auto'} >
+                        <StyledOffcanvasTitle light={light}>
+                            Navigation
+                        </StyledOffcanvasTitle>
+                    </OffcanvasHeader>
+                    <OffcanvasBody>
+                        <OffCanvasA href={'#'}>Page d'acceuil</OffCanvasA>
+                    </OffcanvasBody>
+                </StyledOffCanvas>
+                <Button aria-label={'Primary Navigation Button'} className={"plate plate1 fixed-top " + (show ? ' active' : '')}  onClick={handleShow}>
+                    <Svg show={show ? '0' : '1'} light={light} version="1.1" height="100" width="100" viewBox="0 0 100 100">
                         <path className="line line1" d="M 30,65 H 70"/>
                         <path className="line line2"
                               d="M 70,50 H 30 C 30,50 18.644068,50.320751 18.644068,36.016949 C 18.644068,21.712696 24.988973,6.5812347 38.79661,11.016949 C 52.604247,15.452663 46.423729,62.711864 46.423729,62.711864 L 50.423729,49.152542 L 50.423729,16.101695"/>
                         <path className="line line3"
                               d="M 30,35 H 70 C 70,35 80.084746,36.737688 80.084746,25.423729 C 80.084746,19.599612 75.882239,9.3123528 64.711864,13.559322 C 53.541489,17.806291 54.423729,62.711864 54.423729,62.711864 L 50.423729,49.152542 V 16.101695"/>
-                    </svg>
-                    <svg className="x" version="1.1" height="100" width="100" viewBox="0 0 100 100">
+                    </Svg>
+                    <Svg show={show ? '0' : '1'} className="x" version="1.1" height="100" width="100" viewBox="0 0 100 100">
                         <path className="line" d="M 34,32 L 66,68"/>
                         <path className="line" d="M 66,32 L 34,68"/>
-                    </svg>
-                </div>
-                <Brand className={'navbar-brand'}>Quai Antique</Brand>
+                    </Svg>
+                    <Span>Navigation</Span>
+                </Button>
+            </nav>
+            <Nav light={light} className={'d-flex gap-1 navbar  navbar-expdand-lg border-bottom border-1 pe-2' + (light === '1' ? ' border-white fixed-top' : ' border-primary sticky-top')} >
+                <Brand className={'navbar-brand mx-auto ' + (light === '1' ? 'text-white' : 'text-primary')}>Quai Antique</Brand>
                 <div>
-                    <a href={'#'} className={'btn fs-6 btn-outline-primary rounded-4 border border-2 border-primary  me-2'}>Réserver</a>
-                    <a href={'#'} className={'btn btn-outline-primary rounded-4 border px-2 py-1 border-2 border-primary fs-5'}><i className="bi bi-person-fill"></i></a>
+                    <A light={light} href={'#'} className={'btn fs-6 rounded-4 border border-2 me-2' + (light === '1' ? ' btn-outline-white border-white text-white' : ' btn-outline-primary border-primary text-primary')}>Réserver</A>
+                    <A light={light} href={'#'} className={'btn rounded-4 border px-2 py-1 border-2  fs-5' + (light === '1' ? ' btn-outline-white border-white text-white' : ' btn-outline-primary border-primary text-primary')}><i className="bi bi-person-fill"></i><Span>Se connecter</Span></A>
                 </div>
-                <Offcanvas show={show} onHide={handleShow} className={"d-flex justify-content-center"}>
-                    <OffcanvasHeader className={'mx-auto'} >
-                        <OffcanvasTitle>
-                            Navigation
-                        </OffcanvasTitle>
-                    </OffcanvasHeader>
-                    <OffcanvasBody>
-                        <p>Ici navigation</p>
-                        <a className={'text-primary hoverSecondary'} href={'#'}>Page d'acceuil</a>
-                    </OffcanvasBody>
-                </Offcanvas>
             </Nav>
         </header>
     )
 }
 
 const Nav = styled.nav`
-    background-color: #F7A45840;
+    background-color: ${props => props.light === '1' ? 'rgba(0, 0, 0, 0.5)' : '#FCDBBC'};
     font-family: Merriweather, sans-serif;
     box-shadow: 0px 4px 4px 0px #00000040;
+    z-index: 1025;
 `
 
 const Brand = styled.a`
     font-family: Great Vibes, sans-serif;
     font-size: 2rem !important;
+    pointer-events: none;
 `
+
+const StyledOffCanvas = styled(Offcanvas)`
+    background-color: ${props => props.light === '1' ? 'black' : '#FFFFFF'};
+`
+
+const StyledOffcanvasTitle = styled(OffcanvasTitle)`
+    color: ${props => props.light === '0' ? '' : '#FFFFFF'};
+`
+
+const Svg = styled.svg`
+    stroke: ${props => props.light === '0' ? '#381B1D' : '#FFFFFF'};
+    &:hover {
+        stroke: ${props => props.light === '0' ? '#965B31' : (props.show === '0' ? '#F7A458' : '#381B1D')};
+    };
+    transition: stroke 500ms ease-out;
+    position: absolute;
+    height: 50px;
+    width: 50px;
+    z-index: auto;
+`
+
+const Button = styled.button`
+    height: 50px;
+    width: 50px;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    background-color: transparent;
+    border: none;
+    z-index: 10000;
+    margin-top: 0.8rem;
+`
+
+const Span = styled.span`
+    display: none;
+`
+
+const OffCanvasA = styled.a`
+    color: ${props => props.light === '0' ? '' : '#FFFFFF'};
+    text-decoration: none;
+    display: table;
+    transform: scale(1);
+    transform-origin: left;
+    transition: transform 0.2s ease-in;
+    &:hover {
+        color: ${props => props.light === '0' ? '' : '#F7A458'};
+        transform: scale(1.1);
+    };
+`
+
+const A = styled.a`
+    transition: color 500ms ease-out;
+    transition: background-color 500ms ease-out;
+    &:hover {
+        color: ${props => props.light === '1' ? '#381B1D' : '#FFFFFF'} !important;
+        background-color: ${props => props.light === '1' ? '#FFFFFF' : '#965B31'} !important;
+    };
+`
+
--- assets/react/Routes/Home.jsx
+++ assets/react/Routes/Home.jsx
@@ -1,8 +1,33 @@
 import React from "react";
 import Header from "../Components/Header";
+import Footer from "../Components/Footer";
+import img from '../../images/Homepage-1920.jpg';
+import { Carousel, CarouselItem } from "react-bootstrap";
+import styled from "styled-components";
 
 export default function () {
     return (
-        <Header />
+        <>
+            <Header light={'1'}/>
+            <main>
+                <Carousel>
+                    <CarouselItem className={'min-vh-100'}>
+                        <Img
+                            className={'d-block vw-100 vh-100'}
+                            src={img}
+                            alt={'Plat number 1'}
+                        />
+                        <Carousel.Caption >
+                            <h3>Plat number 1</h3>
+                        </Carousel.Caption>
+                    </CarouselItem>
+                </Carousel>
+            </main>
+            <Footer />
+        </>
     )
 }
+
+const Img = styled.img`
+    object-fit: cover;
+`
